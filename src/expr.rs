df364c7a12d9a8f7298432af162f9abd769f608c
InnocentusLime <innocentuslime.help@gmail.com>
2025-01-01 15:56:39 +0200

    Add conversion

--- src/expr.rs
+++ src/expr.rs
@@ -1,6 +1,12 @@
 pub const BITS_PER_VAL: u32 = 32;
 pub type ExprVal = i32;
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Value {
+    Arg(String),
+    Const(ExprVal),
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Variable {
     Const,
@@ -23,41 +29,44 @@ pub enum BinopKind {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum Expr {
-    Variable(Variable),
-    Unop(UnopKind, Box<Expr>),
+pub enum Expr<V = Variable> {
+    Variable(V),
+    Unop(UnopKind, Box<Expr<V>>),
     Shift {
         is_left: bool,
-        n: Variable,
-        expr: Box<Expr>,
+        n: V,
+        expr: Box<Expr<V>>,
     },
-    Binop(BinopKind, Box<(Expr, Expr)>),
+    Binop(BinopKind, Box<(Expr<V>, Expr<V>)>),
 }
 
 impl Expr {
     // NOTE: if stack starts overfilling -- that should be the first
     // function we turn non-recursive.
-    pub fn walk_expr<T, V, U, S, B>(
+    pub fn walk_expr<T, V, U, S, B, Var, Prom>(
         &self,
         var_action: &mut V,
         unop_action: &mut U,
         shift_action: &mut S,
         binop_action: &mut B,
+        var_promote: &mut Prom,
     ) -> T
     where
-        V: FnMut(Variable) -> T,
+        V: FnMut(Variable) -> Var,
         U: FnMut(UnopKind, T) -> T,
-        S: FnMut(bool, T, T) -> T,
+        S: FnMut(bool, T, Var) -> T,
         B: FnMut(BinopKind, T, T) -> T,
+        Prom: FnMut(Var) -> T,
     {
         match self {
-            Expr::Variable(variable) => var_action(*variable),
+            Expr::Variable(variable) => var_promote(var_action(*variable)),
             Expr::Unop(unop_kind, expr) => {
                 let expr = expr.walk_expr(
                     var_action,
                     unop_action,
                     shift_action,
                     binop_action,
+                    var_promote,
                 );
 
                 unop_action(*unop_kind, expr)
@@ -68,6 +77,7 @@ impl Expr {
                     unop_action,
                     shift_action,
                     binop_action,
+                    var_promote,
                 );
                 let n = var_action(*n);
 
@@ -78,14 +88,16 @@ impl Expr {
                     var_action,
                     unop_action,
                     shift_action,
-                    binop_action
+                    binop_action,
+                    var_promote,
                 );
 
                 let r = lr.1.walk_expr(
                     var_action,
                     unop_action,
                     shift_action,
-                    binop_action
+                    binop_action,
+                    var_promote,
                 );
 
                 binop_action(*binop_kind, l, r)
@@ -102,7 +114,7 @@ impl Expr {
     {
         self.walk_expr(
             &mut var_map,
-            &mut |unop_kind, e| match unop_kind {
+            &mut |unop_kind, e: i32| match unop_kind {
                 UnopKind::Not => -e,
                 UnopKind::Negate => !e,
             },
@@ -118,6 +130,7 @@ impl Expr {
                 BinopKind::Plus => l + r,
                 BinopKind::Minus => l - r,
             },
+            &mut |x| x,
         )
     }
 
@@ -131,7 +144,7 @@ impl Expr {
     {
         self.walk_expr(
             &mut move |v| var_map(ctx, v),
-            &mut |unop_kind, e| match unop_kind {
+            &mut |unop_kind, e: z3::ast::BV<'ctx>| match unop_kind {
                 UnopKind::Not => !e,
                 UnopKind::Negate => -e,
             },
@@ -147,6 +160,29 @@ impl Expr {
                 BinopKind::Plus => l + r,
                 BinopKind::Minus => l - r,
             },
+            &mut |x| x,
+        )
+    }
+
+    pub fn to_ans<V>(
+        &self,
+        mut var_map: V,
+    ) -> Expr<Value>
+    where
+        V: FnMut(Variable) -> Value,
+    {
+        self.walk_expr(
+            &mut |v| var_map(v),
+            &mut |unop_kind, e| {
+                Expr::Unop(unop_kind, Box::new(e))
+            },
+            &mut |is_left, e, n| {
+                Expr::Shift { is_left, n, expr: Box::new(e) }
+            },
+            &mut |binop_kind, l, r| {
+                Expr::Binop(binop_kind, Box::new((l, r)))
+            },
+            &mut |x| Expr::Variable(x),
         )
     }
 }
